f594b880fca0729541f1fa7cb589c07f7207aff8
alothmanmj <alothman.maj@gmail.com>
2023-02-05 14:33:48 +0300

    Build the reader

--- RandomGroups51.java
+++ RandomGroups51.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class RandomGroups51{
     public static void main(String[] args) {
         System.out.println("Working Fine");
+
+        ArrayList<String> arr  = readStudents();
+        System.out.println(arr);
+    }
+    public static ArrayList<String> readStudents(){
+        ArrayList<String> arr  = new ArrayList<>();
+
+        try {
+            File f = new File("Student List.txt");
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()){
+                arr.add(sc.nextLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return arr;
     }
 }
